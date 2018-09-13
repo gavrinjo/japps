948f9ccd21d49361b5b1d95f55800fecd9f64663
Gavrinjo <gavrinjo@gmx.com>
2018-09-13 23:57:47 +0200

    web structure and templates added, added routs to the script

--- flaskdemo.py
+++ flaskdemo.py
@@ -1,16 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
+posts = [
+    {
+        "author": "Drazen",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": "1. prosinca, 2018"
+    },
+    {
+        "author": "Ena",
+        "title": "Blog post 2",
+        "content": "Second post content",
+        "date_posted": "2. prosinca, 2018"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page!!</h1>"
+    return render_template("home.html", posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<h1>About Page</h1>"
+    return render_template("about.html", title="About")
 
 
 if __name__ == "__main__":
