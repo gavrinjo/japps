87d1efecf8768154b8ac7dfa9d12ba8021357627
Gavrinjo <gavrinjo@gmx.com>
2018-10-01 20:28:06 +0200

    new templates added login/register forms added updated layout page updated app script

--- flaskdemo.py
+++ flaskdemo.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, url_for
-from bs4 import BeautifulSoup as bs
-from requests import get
-import re
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistarationForm, LoginForm
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "3eccb7be0b8fab6d1a7d4a70414cebd0"
 
 posts = [
     {
@@ -32,6 +32,26 @@ def home():
 def about():
     return render_template("about.html", title="About")
 
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistarationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}!", "success")
+        return redirect(url_for("home"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "admin@blog.com" and form.password.data == "admin":
+            flash("You have been loged in", "success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login Unsuccessful. Please check your email and password", "danger")
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
